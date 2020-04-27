bd987ff2714999d65209dabe8aa195f0a19dd5a3
Amrut <amrutpanda7789@gmail.com>
2020-04-27 21:02:23 +0530

    arithmetic expressions are added

--- ArithmeticComputation/ArithmeticComputationAndSorting.sh
+++ ArithmeticComputation/ArithmeticComputationAndSorting.sh
@@ -5,3 +5,9 @@ read -p "Enter second number:: " b
 read -p "Enter third number:: " c
 
 
+result1=$(( $a + $b * $c ))
+result2=$(( $a * $b + $c ))
+result3=$(( $c + $a / $b ))
+result4=$(( $a % $b + $c ))
+
+
