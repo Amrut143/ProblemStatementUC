65872b3970e0652db74c15aca437c7effbb24a21
Amrut <amrutpanda7789@gmail.com>
2020-04-27 23:41:28 +0530

    employee daily wage java file is added

--- EmpWageUsingJava/EmpWageBuilder.java
+++ EmpWageUsingJava/EmpWageBuilder.java
@@ -3,11 +3,20 @@ public class EmpWageBuilder {
 		public static void main(String[] args) {
 			//constants
 			int IS_FULL_TIME = 1;
+			int emp_rate_per_hr=20;
+			int emp_hr=8;
+			double salary;
 					//computation
 			double empCheck = Math.floor(Math.random() * 10) % 2;
-			if (empCheck == IS_FULL_TIME)
-				System.out.println("Employee is present");
-			else
-				System.out.println("Employee is abscent");
+			if (empCheck == IS_FULL_TIME){
+				//salary computation
+				salary=emp_hr*emp_rate_per_hr;
+
+				System.out.println("Employee Daily Wage is::"+salary);
+			}
+			else{
+				salary=0;
+				System.out.println("Employee Daily Wage is::"+salary);
+			}
 		}
 }
