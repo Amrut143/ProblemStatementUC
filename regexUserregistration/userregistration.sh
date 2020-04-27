e0e75aaf0c0acc561836706e25ca9f0ec1af3160
Amrut <amrutpanda7789@gmail.com>
2020-04-27 22:13:17 +0530

    user email is added

--- regexUserregistration/userregistration.sh
+++ regexUserregistration/userregistration.sh
@@ -20,3 +20,17 @@ then
 else
 	echo invalid
 fi
+echo "Enter email address::"
+read Email
+pat="^[0-9a-zA-Z]+"
+pat="@[0-9a-zA-Z]+"
+pat=".[a-zA-Z]{2,4}"
+pat="([_+-.][a-zA-Z0-9]+)*"
+pat="([.][A-Za-z]{3})$"
+pat="^[0-9a-zA-Z]+([-_+.][0-9a-zA-Z]+)*@[0-9A-Za-z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{3})*$"
+if [[ $Email =~ $pat ]]
+then
+	echo valid email address
+else
+	echo invalid email address
+fi
