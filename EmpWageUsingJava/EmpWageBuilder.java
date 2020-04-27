059556e6a1ea72a6583c95c093a8809f1d509e73
Amrut <amrutpanda7789@gmail.com>
2020-04-28 01:44:40 +0530

    employee wage for month java file is added

--- EmpWageUsingJava/EmpWageBuilder.java
+++ EmpWageUsingJava/EmpWageBuilder.java
@@ -6,28 +6,33 @@ public class EmpWageBuilder {
 			double IS_PART_TIME = 2;
 			int emp_rate_per_hr=20;
 			int emp_hr;
-			double salary;
+			double totalSalary=0;
+			int num_Working_days=20;
+			double salary=0;
 					//computation
+		for (int day=1; day<=num_Working_days; day++)
+			{
 			double random = Math.floor((double)Math.random() * 10) % 3;
 			int empCheck=(int)Math.round(random);
 			switch (empCheck){
 
 				case 1:
 				emp_hr=8;
-				System.out.println("Employee is full time");
 				break;
 
 				case 2:
 				emp_hr=4;
-				System.out.println("Employee is part time");
 				break;
 
 				default:
 				emp_hr=0;
-				System.out.println("Employee is abscent");
 				}
 				salary=emp_hr*emp_rate_per_hr;
+				totalSalary=totalSalary+salary;
+			}
 				System.out.println("Employee wage is::"+salary);
+            System.out.println("Employee salary for month is::"+totalSalary);
+
 		}
 }
 
