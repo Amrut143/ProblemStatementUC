819c758b89b55c988772e70764a22e9ef475cf8f
Amrut <amrutpanda7789@gmail.com>
2020-04-28 00:42:00 +0530

    employee part time and wage java fle is added

--- EmpWageUsingJava/EmpWageBuilder.java
+++ EmpWageUsingJava/EmpWageBuilder.java
@@ -3,20 +3,28 @@ public class EmpWageBuilder {
 		public static void main(String[] args) {
 			//constants
 			int IS_FULL_TIME = 1;
+			int IS_PART_TIME = 2;
 			int emp_rate_per_hr=20;
-			int emp_hr=8;
+			int emp_hr;
 			double salary;
 					//computation
-			double empCheck = Math.floor(Math.random() * 10) % 2;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
 			if (empCheck == IS_FULL_TIME){
-				//salary computation
-				salary=emp_hr*emp_rate_per_hr;
 
-				System.out.println("Employee Daily Wage is::"+salary);
+				emp_hr=8;
+
+				System.out.println("Employee is full time");
+			}
+			else if (empCheck == IS_PART_TIME){
+				emp_hr=4;
+				System.out.println("Employee is part time");
 			}
 			else{
-				salary=0;
-				System.out.println("Employee Daily Wage is::"+salary);
+				emp_hr=0;
+				System.out.println("Employee is abscent");
 			}
+				salary=emp_hr*emp_rate_per_hr;
+				System.out.println("Employee wage is::"+salary);
 		}
 }
+
