ff010a9e46153b36e765c2631bef980ca3b59ad1
Amrut <amrutpanda7789@gmail.com>
2020-04-27 20:36:48 +0530

    employee wage using function is added

--- EmpWage/empWageComputation.sh
+++ EmpWage/empWageComputation.sh
@@ -2,25 +2,30 @@
 
 isFullTime=1;
 isPartTime=2;
-MAX_HR_IN_MONTH=10
+MAX_HR_IN_MONTH=4;
 empRatePerHr=20;
 numWorkingDays=20;
 
 totalEmpHr=0;
 totalWorkingDays=0;
 
+function getWorkingHours() {
+		case $1 in
+					$isFullTime)
+							workHours=8 ;;
+					$isPartTime)
+							workHours=4 ;;
+					*)
+							workHours=0 ;;
+		esac
+		echo $workHours
+}
+
+
 while (( $totalEmpHr<$MAX_HR_IN_MONTH && $totalWorkingDays<$numWorkingDays ))
 do
-	((totalWorkingDays++))
-	empCheck=$((RANDOM%3))
-		case $empCheck in
-					$isFullTime)
-							empHr=8 ;;
-					$isPartTime)
-							empHr=4 ;;
-					*)
-							empHr=0 ;;
-		esac
-		totalEmpHr=$(($totalEmpHr+$empHr))
+   ((totalWorkingDays++))
+	workHours="$( getWorkingHours $((RANDOM%3)) )"
+	totalWorkHours=$(($totalWorkHours+$workHours))
 done
-totalSalary=$(($totalEmpHr*$empRatePerHr));
+totalSalary=$(($totalWorkHours*$empRatePerHr));
