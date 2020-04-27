a7d41e540bd3cccf875c6da5b1f4f63fa5b0faf4
Amrut <amrutpanda7789@gmail.com>
2020-04-28 01:20:00 +0530

    employee wage using case is added

--- EmpWageUsingJava/EmpWageBuilder.java
+++ EmpWageUsingJava/EmpWageBuilder.java
@@ -2,27 +2,30 @@ public class EmpWageBuilder {
 
 		public static void main(String[] args) {
 			//constants
-			int IS_FULL_TIME = 1;
-			int IS_PART_TIME = 2;
+			double IS_FULL_TIME = 1;
+			double IS_PART_TIME = 2;
 			int emp_rate_per_hr=20;
 			int emp_hr;
 			double salary;
 					//computation
-			double empCheck = Math.floor(Math.random() * 10) % 3;
-			if (empCheck == IS_FULL_TIME){
+			double random = Math.floor((double)Math.random() * 10) % 3;
+			int empCheck=(int)Math.round(random);
+			switch (empCheck){
 
+				case 1:
 				emp_hr=8;
-
 				System.out.println("Employee is full time");
-			}
-			else if (empCheck == IS_PART_TIME){
+				break;
+
+				case 2:
 				emp_hr=4;
 				System.out.println("Employee is part time");
-			}
-			else{
+				break;
+
+				default:
 				emp_hr=0;
 				System.out.println("Employee is abscent");
-			}
+				}
 				salary=emp_hr*emp_rate_per_hr;
 				System.out.println("Employee wage is::"+salary);
 		}
