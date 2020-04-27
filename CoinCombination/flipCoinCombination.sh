fd8978db56a396c26e6f5b1beab4449ed279a88a
Amrut <amrutpanda7789@gmail.com>
2020-04-27 21:46:56 +0530

    singlet combination is added

---
+++ CoinCombination/flipCoinCombination.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+read -p "Enter the number of flip you want::" num
+headsCount=0
+tailsCount=0
+for (( i=1; i<=$num; i++ ))
+do
+	flip=$(($((RANDOM%10))%2))
+	if [ $flip -eq 0 ]
+	then
+		echo "Heads"
+		headsCount=$(($headsCount+1))
+	else
+		echo "Tails"
+		tailsCount=$(($tailsCount+1))
+	fi
+done
+		echo "numbers of head is::"$headsCount
+		echo "number of tail is::" $tailsCount
+
+		percentageOfHeads=$(($(($headsCount*100))/$num))
+		percentageOfTails=$(($(($tailsCount*100))/$num))
+
+		echo "Heads=$percentageOfHeads%"
+		echo "Tails=$percentageOfTails%"
+		dict[singlet]="{Heads=$headsCount:percentage=$percentageOfHeads Tails=$tailsCount:percentage=$percentageOfTails}"
+		echo ${dict[@]}
