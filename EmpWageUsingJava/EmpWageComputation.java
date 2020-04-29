4cdd4e2c77681aef7c4daadd22f395131b64f86d
Amrut143 <63304164+Amrut143@users.noreply.github.com>
2020-04-29 18:32:47 +0530

    employee daily wage is added

--- EmpWageUsingJava/EmpWageComputation.java
+++ EmpWageUsingJava/EmpWageComputation.java
@@ -1,39 +1,55 @@
 /**
-* Employee wage computation program
-* @author: Amrut
+*Employee wage computation program
+*@author:Amrut
 */
 import java.util.Random;
+public class EmpWageComputation 
+{
+	//main method
+	public static void main(String[] args) 
+	{
+		int attend;
+		System.out.println("Welcome to employee wage computation");
 
-public class EmpWageComputation {
-		//main method
-      public static void main(String[] args) {
+		//instantiating EmpUtil class
+		EmpUtil eu=new EmpUtil();
+		Random ran=new Random();
+		attend=ran.nextInt(2);
 
-         //create EmpUtil object
-      final EmpUtil emp=new EmpUtil();
-		//call daily wage method
-		System.out.println("Employee daily wage is"+emp.getDailyWage());
-   }
+		//calling checkAttendance method
+	  	eu.checkAttendance(attend);
+	}
 }
-class EmpUtil {
-		//constants
-		int empRatePerHr=20;
-		int empHr=8;
-		double salary;
-      //assign variable to check emp present or abscent
-      double empCheck = Math.floor((double)Math.random() * 10)% 2;
 
-		//method for emp daily wage computation
-		public double getDailyWage() {
-				if (empCheck == 1)
-				{
-					System.out.println("Employee is present");
-					salary=empHr*empRatePerHr;
-				}
-				else
-				{
-					System.out.println("Employee is abscent");
-					salary=0;
-				}
-				return salary;
+//Employee utility class
+class EmpUtil
+{
+	int attendance;
+	double salary;
+	final int wagePerHr=20;
+	int empHr;
+
+	//method to check employee attendance
+	public void checkAttendance(int attendance)
+	{
+		this.attendance = attendance;
+		if(this.attendance==0)
+		{
+			System.out.println("Employee is present");
+			empDailyWage();
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+	}
+	
+	//method to calculate employee daily wage
+	public void empDailyWage()
+	{
+		empHr=8;
+		salary=wagePerHr*empHr;
+		System.out.println("Employee daily wage is::"+salary);
+		
 	}
 }
